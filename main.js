4eac202646f6aa407b4a5cb2098b7411fb64c60c
Chong Xie <chongx@andrew.cmu.edu>
2012-09-06 17:58:23 -0400

    Completely different.

--- main.js
+++ main.js
@@ -105,9 +105,9 @@ var game = {
     ctx.font = "30px Tahoma, Geneva";
     ctx.textAlign = "center";
     ctx.fillStyle = "black";
-    ctx.fillText("You scored " + this.score + " points", canvas.width / 2, 50);
-    if (this.score > this.highscores[this.difficulty]) {
-      this.highscores[this.difficulty] = this.score;
+    ctx.fillText("You survived for " + this.run.time + " seconds", canvas.width / 2, 50);
+    if (this.run.time > this.highscores[this.difficulty]) {
+      this.highscores[this.difficulty] = this.run.time;
       ctx.fillText("NEW HIGHSCORE!", canvas.width / 2, 95);
     }
     ctx.font = "14px Tahoma, Geneva";
@@ -122,9 +122,9 @@ var game = {
     ctx.fillStyle = "black";
     ctx.fillText("Highscores", canvas.width / 2, 50);
     ctx.font = "14px Tahoma, Geneva";
-    ctx.fillText("Easy: " + this.highscores[0], canvas.width / 2, 100);
-    ctx.fillText("Medium: " + this.highscores[1], canvas.width / 2, 150);
-    ctx.fillText("Hard: " + this.highscores[2], canvas.width / 2, 200);
+    ctx.fillText("Easy: " + this.highscores[0].toFixed(1) + " seconds", canvas.width / 2, 100);
+    ctx.fillText("Medium: " + this.highscores[1].toFixed(1) + " seconds", canvas.width / 2, 150);
+    ctx.fillText("Hard: " + this.highscores[2].toFixed(1) + " seconds", canvas.width / 2, 200);
     ctx.fillText("Click anywhere to continue", canvas.width / 2, 350);
   },
 
@@ -136,22 +136,24 @@ var game = {
     ctx.fillStyle = "black";
     ctx.fillText("Instructions", canvas.width / 2, 50);
     ctx.font = "14px Tahoma, Geneva";
-    ctx.fillText("When the game starts, you will see a number of shape outlines on the screen", canvas.width / 2, 100);
-    ctx.fillText("In the top right corner, you will see the next shape you can use", canvas.width / 2, 130);
-    ctx.fillText("Place each shape as accurately as possible into its corresponding outline", canvas.width / 2, 160);
-    ctx.fillText("The color of the outline indicates the color of the shape you need to use", canvas.width / 2, 190);
-    ctx.fillText("Use the 1,2,3,4 keys to use blue, red, green, and purple respectively", canvas.width / 2, 220);
+    ctx.fillText("When the game starts, shape outlines will start appearing on the screen", canvas.width / 2, 100);
+    ctx.fillText("In the top right corner, you will see the current shape you can use", canvas.width / 2, 125);
+    ctx.fillText("Place each shape as accurately as possible into a matching outline", canvas.width / 2, 150);
+    ctx.fillText("Each time you do, you will get more time depending on your accuracy", canvas.width / 2, 175);
+    ctx.fillText("When your time runs out, the game will end.", canvas.width / 2, 200);
+    ctx.fillText("The color of the outline indicates the color of the shape you need to use", canvas.width / 2, 225);
+    ctx.fillText("Use the 1,2,3,4 keys to use blue, red, green, and purple respectively", canvas.width / 2, 250);
     ctx.fillStyle = "rgb(13, 92, 148)";
-    ctx.fillText("blue", 276, 220);
+    ctx.fillText("blue", 276, 250);
     ctx.fillStyle = "rgb(156, 19, 51)";
-    ctx.fillText("red", 307, 220);
+    ctx.fillText("red", 307, 250);
     ctx.fillStyle = "rgb(16, 163, 11)";
-    ctx.fillText("green", 342, 220);
+    ctx.fillText("green", 342, 250);
     ctx.fillStyle = "rgb(103, 13, 148)";
-    ctx.fillText("purple", 413, 220);
+    ctx.fillText("purple", 413, 250);
     ctx.fillStyle = "black";
-    ctx.fillText("Press q and w to rotate your current shape left or right", canvas.width / 2, 250);
-    ctx.fillText("Left-click to place the shape", canvas.width / 2, 280);
+    ctx.fillText("Press q and w to rotate your current shape left or right", canvas.width / 2, 275);
+    ctx.fillText("Left-click to place the shape, there is no penalty to placing shapes", canvas.width / 2, 300);
     ctx.fillText("Click anywhere to continue", canvas.width / 2, 350);
   },
 
@@ -285,7 +287,6 @@ var game = {
         this.run.start();
         break;
       case 3:
-        this.score = this.run.score();
         this.drawEnding();
         break;
       case 4:
@@ -308,53 +309,65 @@ var game = {
 
 // 0 is easy, 1 is medium, 2 is hard
 // Maps difficulty to number of shapes to draw
-var gameDifficulties = [10, 30, 50];
+var gameDifficulties = [{shapeLifetime: 15.0, updateInterval: 500, baseScore: 5}, {shapeLifetime: 10.0, updateInterval: 400, baseScore: 4}, {shapeLifetime: 3.0, updateInterval: 300, baseScore: 2}];
 
 // A Run object keeps track of what the player has done during the current run
 // of a game
 function Run(difficulty) {
   this.playerColor = 0;
-  this.time = 0;
-  this.numShapes = gameDifficulties[difficulty];
+  this.time = 0.0;
+  this.timeleft = 10.0; // Start with 10 seconds
+  this.shapeLifetime = gameDifficulties[difficulty].shapeLifetime;
   this.targetShapes = [];
-  this.playerShapes = [];
-  var usedShapes = [];
-  // Generate a bunch of random shapes
-  for (var i = 0; i < this.numShapes; i++) {
+  this.staleShapes = [];
+  this.playerShape = null;
+  // Generate first player shape
+  this.genPlayerShape();
+  this.notification = null;
+  this.notificationTime = 0;
+}
+
+Run.prototype.updateTargets = function() {
+  var shape = this.genRotatedShape();
+  shape.setOutline(true);
+  this.targetShapes.push(shape);
+  shape.fadeOut(this.shapeLifetime);
+}
+
+Run.prototype.genPlayerShape = function() {
+  var shape = this.genShape();
+  shape.setCoords(560, 30);
+  this.playerShape = shape;
+};
+
+Run.prototype.genShape = function() {
     var shapeType = Math.floor(Math.random() * 3);
-    var x, y, size, color, direction;
-    // Need to make sure no two shapes that are exactly the same are at the same
-    // place (track them in usedShapes)
-    do {
+    var x, y, size, color, direction, shape;
       x = Math.floor(Math.random() * (canvas.width - 150)) + 75;
       y = Math.floor(Math.random() * (canvas.height - 150)) + 100;
       size = Math.floor(Math.random() * shapeSizes.length);
       color = Math.floor(Math.random() * shapeColors.length);
       direction = Math.floor(Math.random() * 8);
-    } while (usedShapes["" + x + y] !== undefined && usedShapes["" + x + y].size !== size);
-    usedShapes["" + x + y] = {size: size};
-    switch (shapeType) {
+   switch (shapeType) {
       case 0:
-        this.targetShapes[i] = new Square(x, y, color, size);
-        this.targetShapes[i].rotate(direction);
-        this.playerShapes[i] = new Square(560, 30, color, size);
+        shape = new Square(x, y, color, size);
         break;
       case 1:
-        this.targetShapes[i] = new Circle(x, y, color, size);
-        this.targetShapes[i].rotate(direction);
-        this.playerShapes[i] = new Circle(560, 30, color, size);
+        shape = new Circle(x, y, color, size);
         break;
       case 2:
-        this.targetShapes[i] = new Semicircle(x, y, color, size);
-        this.targetShapes[i].rotate(direction);
-        this.playerShapes[i] = new Semicircle(560, 30, color, size);
+        shape = new Semicircle(x, y, color, size);
         break;
       default:
         break;
     }
-  }
-  this.current = 0;
-}
+    return shape;
+};
+Run.prototype.genRotatedShape = function() {
+  var shape = this.genShape();
+  shape.rotate(Math.floor(Math.random() * 8));
+  return shape;
+};
 // Keypress handler for changing colors and rotating
 Run.prototype.keypressHandler = function(e) {
   switch (e.keyCode) {
@@ -371,10 +384,10 @@ Run.prototype.keypressHandler = function(e) {
       game.run.playerColor = 3;
       break;
     case 81:
-      game.run.playerShapes[game.run.current].rotate(-1);
+      game.run.playerShape.rotate(-1);
       break;
     case 87:
-      game.run.playerShapes[game.run.current].rotate(1);
+      game.run.playerShape.rotate(1);
       break;
     default:
       break;
@@ -383,18 +396,22 @@ Run.prototype.keypressHandler = function(e) {
 };
 // Click handler for placing shapes
 Run.prototype.clickHandler = function(e) {
-  var shape = game.run.playerShapes[game.run.current];
+  var shape = game.run.playerShape;
   var x = e.pageX - canvas.offsetLeft;
   var y = e.pageY - canvas.offsetTop;
   // Make sure the user doesn't place inside the top stats bar
   if (y > 80) {
     shape.setCoords(x, y);
-    shape.fadeIn(0.0);
-    game.run.current++;
-    game.run.draw();
-    if (game.run.current == game.run.numShapes) {
-      game.run.end();
+    shape.fadeOut(1.0);
+    var score = game.run.getScore(shape);
+    if (score > 0) {
+      game.run.notificationTime = 1.0;
+      game.run.notification = score;
+      game.run.timeleft += score;
     }
+    game.run.staleShapes.push(shape);
+    game.run.genPlayerShape();
+    game.run.draw();
   }
 };
 // Draws the whole game screen
@@ -409,27 +426,30 @@ Run.prototype.drawStats = function() {
   ctx.textAlign = "left";
   ctx.fillStyle = "rgb(60, 60, 60)";
   ctx.clearRect(40, 0, 450, 70);
-  ctx.fillText("Time: " + this.time + " seconds", 50, 30);
-  ctx.fillText(this.current + "/" + this.numShapes + " placed", 280, 30);
-  ctx.fillText("Next: ", 450, 30);
+  ctx.fillText("Time Left: " + this.timeleft.toFixed(1) + " seconds", 50, 30);
+  if (this.notificationTime > 0) {
+    ctx.fillStyle = "rgb(47, 224, 27)";
+    ctx.fillText("+ " + this.notification.toFixed(1), 280, 30);
+    ctx.fillStyle = "rgb(60, 60, 60)";
+  }
+  ctx.fillText("Current shape: ", 350, 30);
   ctx.fillRect(0, 60, canvas.width, 2);
 };
 // Draws just the shape outlines and player shapes
 Run.prototype.drawScreen = function() {
   for (var i in this.targetShapes) {
-    this.targetShapes[i].draw(true);
+    this.targetShapes[i].draw();
   }
-  for (var i = 0; i < this.current; i++) {
-    this.playerShapes[i].draw();
+  for (var i in this.staleShapes) {
+    this.staleShapes[i].draw();
   }
-  if (this.current < this.numShapes) {
-    this.playerShapes[this.current].setColor(this.playerColor);
-    this.playerShapes[this.current].draw();
-  }
+  this.playerShape.setColor(this.playerColor);
+  this.playerShape.draw();
 };
 // Ends the current game
 Run.prototype.end = function() {
   clearInterval(this.timer);
+  clearInterval(this.updater);
   // Clear all draw timeouts
   for (var i in this.timeouts) {
     clearTimeout(this.timeouts[i]);
@@ -440,17 +460,18 @@ Run.prototype.end = function() {
   game.setState(3);
 }
 // Scores the player based on proximity, time, orientation, color, and type
-Run.prototype.score = function() {
-  var score = 0;
-  for (var i in game.run.playerShapes) {
-    var bestScore = 0;
-    var bestShape = null;
-    var playerShape = game.run.playerShapes[i];
+Run.prototype.getScore = function() {
+  var bestScore = 0;
+  var bestShape = null;
+  var playerShape = game.run.playerShape;
     for (var j in game.run.targetShapes) {
       var targetShape = game.run.targetShapes[j]; 
       if (targetShape.color === playerShape.color &&
           targetShape.typeName === playerShape.typeName) {
-        var shapeScore = parseInt((1 - Math.sqrt(Math.pow(Math.abs(targetShape.x - playerShape.x), 2) + Math.pow(Math.abs(targetShape.y - playerShape.y), 2)) / shapeSizes[targetShape.size].width) * 100);
+        var shapeScore = parseInt(
+            (1 - Math.sqrt(Math.pow(Math.abs(targetShape.x - playerShape.x), 2) + Math.pow(Math.abs(targetShape.y - playerShape.y), 2)) /
+            shapeSizes[targetShape.size].width) * 
+            gameDifficulties[game.difficulty].baseScore);
         shapeScore *= targetShape.compareAngle(playerShape.angle);
         if (shapeScore > bestScore) {
           bestScore = shapeScore;
@@ -459,11 +480,9 @@ Run.prototype.score = function() {
       }
     }
     if (bestShape !== null) {
-      score += bestScore;
-      game.run.targetShapes.splice(j, 1);
+      game.run.targetShapes.splice(bestShape, 1);
     }
-  }
-  return Math.ceil(score * Math.ceil(1 / (this.time / 10)));
+  return Math.round(bestScore * 10) / 10;
 }
 // Starts the current game
 Run.prototype.start = function() {
@@ -476,9 +495,20 @@ Run.prototype.start = function() {
   canvas.setAttribute('tabindex','0');
   canvas.focus();
   this.timer = setInterval(function() {
-    game.run.time++;
-    game.run.drawStats();
-  }, 1000);
+    game.run.notificationTime -= 0.1;
+    game.run.timeleft -= 0.1;
+    game.run.timeleft = Math.round(game.run.timeleft * 10) / 10;
+    if (game.run.timeleft < 0.0) {
+      game.run.end();
+    } else {
+      game.run.time += 0.1;
+      game.run.time = Math.round(game.run.time * 10) / 10;
+      game.run.draw();
+    }
+  }, 100);
+  this.updater = setInterval(function() {
+    game.run.updateTargets();
+  }, gameDifficulties[game.difficulty].updateInterval);
 }
 Run.prototype.timeouts = [];
 
@@ -500,7 +530,6 @@ Shape.prototype.angle = 0;
 // Function to fade a shape into view
 Shape.prototype.fadeIn = function(opacity) {
   this.opacity = opacity;
-  var self = this;
   if(opacity < 1.0) {
     game.run.timeouts.push(setTimeout(function(shape, alpha) {
       shape.fadeIn(alpha);
@@ -508,7 +537,35 @@ Shape.prototype.fadeIn = function(opacity) {
   }
   this.draw();
 };
+Shape.prototype.fadeOut = function(time) {
+  this.fadeOutHelper(1.0, parseInt(time * 1000 / 20));
+}
+Shape.prototype.fadeOutHelper = function(opacity, increment) {
+  if (opacity < 0) {
+    opacity = 0;
+  }
+  this.opacity = opacity;
+  if(opacity > 0.0) {
+    game.run.timeouts.push(setTimeout(function(shape, alpha, inc) {
+      shape.fadeOutHelper(alpha, inc);
+    }, increment, this, opacity - 0.05, increment));
+  } else {
+    var index = game.run.staleShapes.indexOf(this);
+    if (index) {
+      game.run.staleShapes.splice(index, 1);
+    }
+
+    var index = game.run.targetShapes.indexOf(this);
+    if (index) {
+      game.run.targetShapes.splice(index, 1);
+    }
+  }
+};
 Shape.prototype.opacity = 1.0;
+Shape.prototype.outline = false;
+Shape.prototype.setOutline = function(outline) {
+  this.outline = outline;
+};
 // Rotates the current shape (can only rotate by Math.PI / 4)
 Shape.prototype.rotate = function(direction) {
   var d = (direction + this.angle / (Math.PI / 4)) % 8;
@@ -536,22 +593,22 @@ Square.prototype.compareAngle = function(angle) {
   }
   return 0.5;
 };
-Square.prototype.draw = function(outline) {
+Square.prototype.draw = function() {
   var width = shapeSizes[this.size].width;
   var height = shapeSizes[this.size].height;
   ctx.translate(this.x, this.y);
   ctx.rotate(this.angle);
   var x = parseInt(-width/2);
   var y = parseInt(-height/2);
-  if (outline) {
+  ctx.globalAlpha = this.opacity;
+  if (this.outline) {
     ctx.strokeStyle = shapeColors[this.color];
     ctx.strokeRect(x, y, width, height);
   } else {
     ctx.fillStyle = shapeColors[this.color];
-    ctx.globalAlpha = this.opacity;
     ctx.fillRect(x, y, width, height);
-    ctx.globalAlpha = 1.0;
   }
+  ctx.globalAlpha = 1.0;
   ctx.setTransform(1, 0, 0, 1, 0, 0);
 }
 Square.prototype.typeName = "square";
@@ -563,13 +620,14 @@ function Circle(x, y, color, size) {
 Circle.prototype = new Shape();
 Circle.prototype.constructor = Circle;
 Circle.prototype.compareAngle = function(angle) { return 1; };
-Circle.prototype.draw = function(outline) {
+Circle.prototype.draw = function() {
   var width = shapeSizes[this.size].width;
   var height = shapeSizes[this.size].height;
   var radius = width / 2;
   var x = this.x;
   var y = this.y;
-  if (outline) {
+  ctx.globalAlpha = this.opacity;
+  if (this.outline) {
     ctx.strokeStyle = shapeColors[this.color];
     ctx.beginPath();
     ctx.arc(x, y, radius, 0, 2 * Math.PI, true);
@@ -577,13 +635,12 @@ Circle.prototype.draw = function(outline) {
     ctx.stroke();
   } else {
     ctx.fillStyle = shapeColors[this.color];
-    ctx.globalAlpha = this.opacity;
     ctx.beginPath();
     ctx.arc(x, y, radius, 0, 2 * Math.PI, true);
     ctx.closePath();
     ctx.fill();
-    ctx.globalAlpha = 1.0;
   }
+  ctx.globalAlpha = 1.0;
   ctx.setTransform(1, 0, 0, 1, 0, 0);
 }
 Circle.prototype.typeName = "circle";
@@ -599,13 +656,14 @@ Semicircle.prototype.compareAngle = function(angle) {
         return 1;
     return 0.2;
 };
-Semicircle.prototype.draw = function(outline) {
+Semicircle.prototype.draw = function() {
   var width = shapeSizes[this.size].width;
   var height = shapeSizes[this.size].height;
   var radius = width / 2;
   var x = this.x;
   var y = this.y;
-  if (outline) {
+  ctx.globalAlpha = this.opacity;
+  if (this.outline) {
     ctx.strokeStyle = shapeColors[this.color];
     ctx.beginPath();
     var startAngle = this.angle;
@@ -614,14 +672,13 @@ Semicircle.prototype.draw = function(outline) {
     ctx.stroke();
   } else {
     ctx.fillStyle = shapeColors[this.color];
-    ctx.globalAlpha = this.opacity;
     ctx.beginPath();
     var startAngle = this.angle;
     ctx.arc(x, y, radius, startAngle, startAngle + Math.PI, true);
     ctx.closePath();
     ctx.fill();
-    ctx.globalAlpha = 1.0;
   }
+  ctx.globalAlpha = 1.0;
   ctx.setTransform(1, 0, 0, 1, 0, 0);
 }
 Semicircle.prototype.typeName = "semicircle";
